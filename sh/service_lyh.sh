ac59ff487532e61f63cb1226e9ba327a3b3f37e1
dayepao <56154142+dayepao@users.noreply.github.com>
2021-01-05 19:10:09 +0800

    Update service_lyh.sh

--- sh/service_lyh.sh
+++ sh/service_lyh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+makeservice(){
 read -p "请输入要创建的service名称：" service_name
 read -p "请输入service要执行的命令：" todo
 cat > /etc/systemd/system/${service_name}.service <<EOF
@@ -17,3 +18,21 @@ EOF
 
 systemctl enable ${service_name}
 systemctl start ${service_name}
+}
+
+while :
+do
+    read -p "是否要添加开机启动项?（y/N）:" skey
+    case ${skey} in
+    [yY])
+        makeservice
+        break 1
+        ;;
+    [nN])
+        break 1
+        ;;
+    *)
+        echo -e "\033[31;1m [错误] \033[0m 请重新输入"
+        ;;
+    esac
+done
