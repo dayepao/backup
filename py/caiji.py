5cf0a5061a9305bb7fb9c4ec7ab6aefc3890e92f
dayepao <56154142+dayepao@users.noreply.github.com>
2021-02-02 15:36:43 +0800

    Update caiji.py

--- py/caiji.py
+++ py/caiji.py
@@ -1,21 +1,25 @@
-import requests,sys,datetime,urllib.parse
+import requests,sys,datetime,urllib.parse,imghdr
 import urllib,os
 from bs4 import BeautifulSoup
 
 def download() :
     key = 1
-    while key < 1765 :
+    while key < 1800 :
         url = "https://tbing.cn/detail/" + str(key)
         res = requests.get(url)
         html = res.text
         soup = BeautifulSoup(html,'html.parser')
         imgs = soup.find_all('img')
         src = imgs[0].get('src')
-        imgr = requests.get(src)
+        headers = {'referer' : 'https://tbing.cn/'}
+        imgr = requests.get(src,headers=headers)
         filename = "bing\\" + str(key) + ".jpg"
         print("正在下载" + filename)
         with open(filename,'wb') as f:
             f.write(imgr.content)
+        if (str(imghdr.what(filename)) == "None") :
+            os.remove(filename)
+            print("已删除" + filename)
         key = key + 1
 if (os.path.exists('bing')) :
     download()
