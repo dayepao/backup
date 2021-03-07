933848af6336612e39876f9ff45e3aebc08e1aec
dayepao <56154142+dayepao@users.noreply.github.com>
2021-03-07 13:51:06 +0800

    update

--- sh/media_check.sh
+++ sh/media_check.sh
@@ -1,8 +1,9 @@
 while :
 do
-    read -p "是否要检测Netflix解锁情况?（y/N）:" nfkey
-    case ${nfkey} in
+    read -p "是否要检测流媒体解锁情况?（y/N）:" mckey
+    case ${mckey} in
     [yY])
+        apt install -y jq
         bash <(curl -sSL "https://github.com/CoiaPrant/MediaUnlock_Test/raw/main/check.sh")
         break 1
         ;;
