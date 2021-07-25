5c2aaf53d827cd808f6a97f43b975771f7468484
dayepao <56154142+dayepao@users.noreply.github.com>
2021-07-25 10:41:35 +0800

    Update 配置powershell代理.ps1

--- powershell/配置powershell代理.ps1
+++ powershell/配置powershell代理.ps1
@@ -3,21 +3,33 @@
 
 
 if (!(Test-Path -Path $PROFILE.AllUsersAllHosts)) {
-  New-Item -ItemType File -Path $PROFILE.AllUsersAllHosts -Force
+    New-Item -ItemType File -Path $PROFILE.AllUsersAllHosts -Force
 }
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.1#how-to-create-a-profile
 
 $content = @"
 function set_proxy_variable {
-  Set-Item Env:http_proxy "http://127.0.0.1:10809"  # 代理地址
-  Set-Item Env:https_proxy "http://127.0.0.1:10809" # 代理地址
-  Write-Output "启动代理"
+    Set-Item Env:http_proxy "http://127.0.0.1:10809"  # http代理地址
+    Write-Output "启动http代理"
+    Set-Item Env:https_proxy "http://127.0.0.1:10809" # https代理地址
+    Write-Output "启动https代理"
 }
 
 function unset_proxy_variable {
-  Remove-Item Env:http_proxy
-  Remove-Item Env:https_proxy
-  Write-Output "停止代理"
+    try {
+        Remove-Item Env:http_proxy -ErrorAction Stop
+        Write-Output "停止http代理"
+    }
+    catch {
+        Write-Output "未开启http代理"
+    }
+    try {
+        Remove-Item Env:https_proxy -ErrorAction Stop
+        Write-Output "停止https代理"
+    }
+    catch {
+        Write-Output "未开启https代理"
+    }
 }
 
 New-Alias -Name setp -Value set_proxy_variable
