6664b6e84c6b5d06ff53f7a15880cf45c0863a90
dayepao <56154142+dayepao@users.noreply.github.com>
2021-07-27 18:36:02 +0800

    upload auto_post

---
+++ sh/wsl_proxy.sh
@@ -0,0 +1,15 @@
+proxy=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
+
+setp(){
+    export http_proxy="http://${proxy}:10809"  # http代理地址
+    echo "启动http代理"
+    export https_proxy="http://${proxy}:10809" # https代理地址
+    echo "启动https代理"
+}
+
+unsetp(){
+    unset http_proxy
+    echo "停止http代理"
+    unset https_proxy
+    echo "停止https代理"
+}
