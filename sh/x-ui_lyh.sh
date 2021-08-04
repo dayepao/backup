a79d81acf5da43de9d39f2de419148d8b6ca6900
dayepao <56154142+dayepao@users.noreply.github.com>
2021-08-04 20:04:22 +0800

    update

--- sh/x-ui_lyh.sh
+++ sh/x-ui_lyh.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 while :
 do
-    read -p "是否要安装 v2-ui ?（y/N）:" v2key
+    read -p "是否要安装 x-ui ?（y/N）:" v2key
     case ${v2key} in
     [yY])
-        wget "https://blog.sprov.xyz/v2-ui.sh" -O v2ui_install.sh
-        bash v2ui_install.sh
+        bash <(curl -Ls https://raw.githubusercontent.com/sprov065/x-ui/master/install.sh)
         break 1
         ;;
     [nN])
