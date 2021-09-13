129a0ab4e8ea300a2bfaea22d248da9758aa4837
dayepao <56154142+dayepao@users.noreply.github.com>
2021-09-13 19:24:37 +0800

    Update aria2_lyh.sh

--- sh/aria2_lyh.sh
+++ sh/aria2_lyh.sh
@@ -4,8 +4,7 @@ do
     read -p "是否要打开aria2管理面板?（y/N）:" arkey
     case ${arkey} in
     [yY])
-        wget "https://raw.githubusercontent.com/P3TERX/aria2.sh/master/aria2.sh" -O aria2.sh
-        bash aria2.sh
+        bash <(curl -sL https://raw.githubusercontent.com/P3TERX/aria2.sh/master/aria2.sh)
         break 1
         ;;
     [nN])
