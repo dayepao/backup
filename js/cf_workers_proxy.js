38c748a17ad7666199b864b39950cee059d4a645
dayepao <56154142+dayepao@users.noreply.github.com>
2022-07-20 18:53:32 +0800

    Rename cf_workers_proxy.js

--- js/cf_workers_proxy.js
+++ js/cf_workers_proxy.js
@@ -516,3 +516,49 @@ async function handleRequest(request) {
 addEventListener("fetch", event => {
     return event.respondWith(handleRequest(event.request))
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 基于 Cloudflare Workers 搭建 UptimeRobot API 代理，以解决官网 API 跨域问题
+
+const handleRequest = async ({ request }) => {
+    let url = new URL(request.url);
+    let response = await fetch('https://api.uptimerobot.com' + url.pathname, request);
+    response = new Response(response.body, response);
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', '*');
+    response.headers.set('Access-Control-Allow-Credentials', 'true');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type,Access-Token');
+    response.headers.set('Access-Control-Expose-Headers', '*');
+    return response;
+  }
+  
+  addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event));
+  });
