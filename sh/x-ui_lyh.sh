5fd2be77552741bb680aa617702852288d2fb034
dayepao <56154142+dayepao@users.noreply.github.com>
2022-07-22 11:14:16 +0800

    Update x-ui_lyh.sh

--- sh/x-ui_lyh.sh
+++ sh/x-ui_lyh.sh
@@ -4,7 +4,7 @@ do
     read -p "是否要安装 x-ui ?(y/N):" v2key
     case ${v2key} in
     [yY])
-        bash <(curl -Ls https://raw.githubusercontent.com/sprov065/x-ui/master/install.sh)
+        bash <(curl -Ls bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh))
         break 1
         ;;
     [nN])
