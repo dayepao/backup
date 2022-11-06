0923b77e9212850d2ce12714c49e90a4ccb52431
dayepao <56154142+dayepao@users.noreply.github.com>
2022-11-06 14:13:30 +0800

    Update fix_ubuntu_zhcn.sh

--- sh/fix_ubuntu_zhcn.sh
+++ sh/fix_ubuntu_zhcn.sh
@@ -4,8 +4,9 @@ do
     read -p "是否要修复Ubuntu中文乱码?(y/N):" fkey
     case ${fkey} in
     [yY])
+        locale-gen en_US.UTF-8
         locale-gen zh_CN.UTF-8
-        update-locale "LANG=zh_CN.UTF-8"
+        update-locale "LANG=en_US.UTF-8"
         locale-gen --purge
         dpkg-reconfigure --frontend noninteractive locales
         break 1
