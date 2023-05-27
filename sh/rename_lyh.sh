3156a78a3fe844c713a8566dbecd766d1ed8e59a
dayepao <56154142+dayepao@users.noreply.github.com>
2023-05-27 12:59:06 +0800

    Update

--- sh/rename_lyh.sh
+++ sh/rename_lyh.sh
@@ -1,61 +1,56 @@
 #!/bin/bash
-restore_name(){
+restore_name() {
     i=0
-    while (( ${i}<${#failure_names[*]} ))
-    do
-        if [[ "${1:2}" == "${failure_names[${i}]}" ]];then
+    while ((${i} < ${#failure_names[*]})); do
+        if [[ "${1:2}" == "${failure_names[${i}]}" ]]; then
             mv "${1}" "${2}"
             echo -e "\033[33m [已还原] \033[0m${2:2}"
             echo ""
         fi
-        i=$((${i}+1))
+        i=$((${i} + 1))
     done
 }
 
-listchange(){
+listchange() {
     echo ""
     echo "列出文件名对应关系: "
-    while read oldmd51 oldmd52
-    do
-        while read newmd51 newmd52
-        do
-            if [ ${oldmd51} == ${newmd51} ];then
+    while read oldmd51 oldmd52; do
+        while read newmd51 newmd52; do
+            if [ ${oldmd51} == ${newmd51} ]; then
                 echo -e "${oldmd52:2} \033[32;1m >>> \033[0m ${newmd52:2}"
                 restore_name "${newmd52}" "${oldmd52}"
                 break 1
             fi
-        done < "${rootpath}/${videoname}_${season:0-2}_new.txt"
-    done < "${rootpath}/${videoname}_${season:0-2}_old.txt"
+        done <"${rootpath}/${videoname}_${season:0-2}_new.txt"
+    done <"${rootpath}/${videoname}_${season:0-2}_old.txt"
     echo ""
 }
 
-checkmd5(){
+checkmd5() {
     stopkeys=$(sed -n '$=' "${rootpath}/${videoname}_${season:0-2}_new.txt")
     losekey=0
     IFS=$'  '
-    while read oldmd51 oldmd52
-    do
+    while read oldmd51 oldmd52; do
         stopkey=0
-        while read newmd51 newmd52
-        do
-            if [ ${oldmd51} == ${newmd51} ];then
+        while read newmd51 newmd52; do
+            if [ ${oldmd51} == ${newmd51} ]; then
                 break 1
             else
-                stopkey=$(($stopkey+1))
+                stopkey=$(($stopkey + 1))
             fi
-            if [ ${stopkey} -ge ${stopkeys} ];then
+            if [ ${stopkey} -ge ${stopkeys} ]; then
                 echo -e "\033[31;1m [警告] \033[0m ${oldmd52} 可能已经丢失"
                 losekey=1
             fi
-        done < "${rootpath}/${videoname}_${season:0-2}_new.txt"
-    done < "${rootpath}/${videoname}_${season:0-2}_old.txt"
-    if [ ${losekey} == 1 ] || [ ${failurekey} == 1 ];then
+        done <"${rootpath}/${videoname}_${season:0-2}_new.txt"
+    done <"${rootpath}/${videoname}_${season:0-2}_old.txt"
+    if [ ${losekey} == 1 ] || [ ${failurekey} == 1 ]; then
         listchange
     fi
     IFS=$'\r\n'
 }
 
-deleteuseless(){
+deleteuseless() {
     rm -rf *.torrent
     rm -rf *.txt
     rm -rf *.url
@@ -67,10 +62,9 @@ deleteuseless(){
     rm -rf *.png
 }
 
-file_rename_1_0(){
+file_rename_1_0() {
     oldfiles=$(ls $folder)
-    for oldfile in $oldfiles
-    do
+    for oldfile in $oldfiles; do
         newfile=$(echo $oldfile | sed "s/BDE4//g")
         newfile=$(echo $newfile | sed "s/(//g")
         newfile=$(echo $newfile | sed "s/)//g")
@@ -84,182 +78,164 @@ file_rename_1_0(){
         newfile=$(echo $newfile | sed "s/e/e0/g")
         newfile=$(echo $newfile | sed "s/00/0/g")
         newfilekey=10
-        while [ ${newfilekey} -lt '100' ]
-        do
+        while [ ${newfilekey} -lt '100' ]; do
             newfile=$(echo $newfile | sed "s/0${newfilekey}/${newfilekey}/g")
-            newfilekey=$((${newfilekey}+1))
+            newfilekey=$((${newfilekey} + 1))
         done
-        if [ "${oldfile}" != "${newfile}" ] && [ "${oldfile}" != "$0" ] && [ "${oldfile}" != "rename_lyh" ];then
+        if [ "${oldfile}" != "${newfile}" ] && [ "${oldfile}" != "$0" ] && [ "${oldfile}" != "rename_lyh" ]; then
             mv "${oldfile}" "${newfile}"
         fi
     done
 }
 
-file_rename_2_0(){
-    for oldfile in $(ls)
-    do
-        newfile=${oldfile,,}  # 变量字母转为小写
-        if [[ "${oldfile}" != "${newfile}" ]];then
+file_rename_2_0() {
+    for oldfile in $(ls); do
+        newfile=${oldfile,,} # 变量字母转为小写
+        if [[ "${oldfile}" != "${newfile}" ]]; then
             mv "${oldfile}" "${newfile}1"
             mv "${newfile}1" "${newfile}"
         fi
     done
 
     oldfiles=()
-    key=0  # 文件名长度
+    key=0 # 文件名长度
 
-    for oldfile in $(ls)
-    do
+    for oldfile in $(ls); do
         oldfiles[${#oldfiles[*]}]=${oldfile}
-        if (( ${key}==0 ));then
+        if ((${key} == 0)); then
             key=${#oldfile}
         fi
-        if (( ${#oldfile}!=${key} ));then
+        if ((${#oldfile} != ${key})); then
             file_rename_1_0
             return
         fi
     done
 
     j=1
-    while (( ${j}<=${key} ))
-    do
+    while ((${j} <= ${key})); do
         i=0
         temp_name=""
-        while (( ${i}<${#oldfiles[*]} ))
-        do
-            if [[ $temp_name == "" ]];then
+        while ((${i} < ${#oldfiles[*]})); do
+            if [[ $temp_name == "" ]]; then
                 temp_name="${oldfiles[${i}]::${j}}"
             fi
-            if [[ "${oldfiles[${i}]::${j}}" != "$temp_name" ]];then
+            if [[ "${oldfiles[${i}]::${j}}" != "$temp_name" ]]; then
                 break 2
             fi
-            i=$((${i}+1))
+            i=$((${i} + 1))
         done
-        j=$((${j}+1))
+        j=$((${j} + 1))
     done
-
 
     k=${key}
-    while (( ${k}>=0 ))
-    do
+    while ((${k} >= 0)); do
         i=0
         temp_name=""
-        while (( ${i}<${#oldfiles[*]} ))
-        do
-            if [[ $temp_name == "" ]];then
+        while ((${i} < ${#oldfiles[*]})); do
+            if [[ $temp_name == "" ]]; then
                 temp_name="${oldfiles[${i}]:${k}}"
             fi
-            if [[ "${oldfiles[${i}]:${k}}" != "$temp_name" ]];then
+            if [[ "${oldfiles[${i}]:${k}}" != "$temp_name" ]]; then
                 break 2
             fi
-            i=$((${i}+1))
+            i=$((${i} + 1))
         done
-        k=$((${k}-1))
+        k=$((${k} - 1))
     done
 
-    start_index=$(($j-1))
-    index_length=$((${k}-$j+2))
-    if (( ${start_index} >= ${key} ));then
+    start_index=$(($j - 1))
+    index_length=$((${k} - $j + 2))
+    if ((${start_index} >= ${key})); then
         start_index=0
         index_length=${key}
     fi
 
     failurekey_2_0=0
     i=0
-    while (( ${i}<${#oldfiles[*]} ))
-    do
-        if [[ ${oldfiles[${i}]:${start_index}:${index_length}} =~ [^0-9] ]] || [[ ${oldfiles[${i}]:$((${start_index}-1)):1} =~ [1-9] ]];then
+    while ((${i} < ${#oldfiles[*]})); do
+        if [[ ${oldfiles[${i}]:${start_index}:${index_length}} =~ [^0-9] ]] || [[ ${oldfiles[${i}]:$((${start_index} - 1)):1} =~ [1-9] ]]; then
             failurekey_2_0=1
             break
         fi
-        i=$((${i}+1))
+        i=$((${i} + 1))
     done
 
-    if [[ ${failurekey_2_0} == 0 ]];then
+    if [[ ${failurekey_2_0} == 0 ]]; then
         i=0
-        while (( ${i}<${#oldfiles[*]} ))
-        do
-            if [[ "${oldfiles[${i}]}" != "${oldfiles[${i}]:${start_index}:${index_length}}.mp4" ]];then
+        while ((${i} < ${#oldfiles[*]})); do
+            if [[ "${oldfiles[${i}]}" != "${oldfiles[${i}]:${start_index}:${index_length}}.mp4" ]]; then
                 mv "${oldfiles[${i}]}" "${oldfiles[${i}]:${start_index}:${index_length}}.mp4"
             fi
-            i=$((${i}+1))
+            i=$((${i} + 1))
         done
     else
         file_rename_1_0
     fi
 }
 
-
-rename_1_0(){
+rename_1_0() {
     file_rename_2_0
     filenames=$(ls $folder)
-    for filename in $filenames
-    do
+    for filename in $filenames; do
         j=1
-        while :
-        do
-            if [ $j -lt '10' ];then
+        while :; do
+            if [ $j -lt '10' ]; then
                 jj="0${j}"
-                if [[ ${filename} =~ "e${jj}" ]];then
-                mv "${filename}" "${jj}.mp4"
-                break 1
+                if [[ ${filename} =~ "e${jj}" ]]; then
+                    mv "${filename}" "${jj}.mp4"
+                    break 1
                 fi
-            elif [ $j -lt '100' ];then
-                if [[ ${filename} =~ "e${j}" ]];then
-                mv "${filename}" "${j}.mp4"
-                break 1
+            elif [ $j -lt '100' ]; then
+                if [[ ${filename} =~ "e${j}" ]]; then
+                    mv "${filename}" "${j}.mp4"
+                    break 1
                 fi
             else
                 echo -e "\033[31;1m [失败] \033[0m${filename}"
                 break 1
             fi
-            j=$(($j+1))
+            j=$(($j + 1))
         done
     done
 }
 
-
-rename_2_0(){
+rename_2_0() {
     cd rename_lyh
     rootpath=$(pwd)
     deleteuseless
     videonames=$(ls $folder)
-    for videoname in $videonames
-    do
+    for videoname in $videonames; do
         cd "${videoname}"
         deleteuseless
         seasons=$(ls $folder)
-        for season in $seasons
-        do
+        for season in $seasons; do
             echo " [正在处理] ${videoname}/${season}"
             cd "${season}"
             deleteuseless
-            find ./ -type f -print0 | xargs -0 md5sum | sort > "${rootpath}/${videoname}_${season:0-2}_old.txt"
+            find ./ -type f -print0 | xargs -0 md5sum | sort >"${rootpath}/${videoname}_${season:0-2}_old.txt"
             file_rename_2_0
             filenames=$(ls $folder)
             failurekey=0
             failure_names=()
-            if [ ${season} == "Specials" ];then
+            if [ ${season} == "Specials" ]; then
                 SXX="SP"
-                for filename in $filenames
-                do
+                for filename in $filenames; do
                     j=0
-                    while :
-                    do
-                        if [ $j -lt '10' ];then
+                    while :; do
+                        if [ $j -lt '10' ]; then
                             jj="0${j}"
-                            if [[ ${filename} =~ "e${jj}" ]];then
+                            if [[ ${filename} =~ "e${jj}" ]]; then
                                 mv "${filename}" "${videoname} ${SXX}${jj}.mp4"
                                 break 1
-                            elif [[ ${filename} == "${jj}.mp4" ]] || [[ ${filename} == "${jj}.mkv" ]] || [[ ${filename} == "${j}.mp4" ]];then
+                            elif [[ ${filename} == "${jj}.mp4" ]] || [[ ${filename} == "${jj}.mkv" ]] || [[ ${filename} == "${j}.mp4" ]]; then
                                 mv "${filename}" "${videoname} ${SXX}${jj}.mp4"
                                 break 1
                             fi
-                        elif [ $j -lt '100' ];then
-                            if [[ ${filename} =~ "e${j}" ]];then
+                        elif [ $j -lt '100' ]; then
+                            if [[ ${filename} =~ "e${j}" ]]; then
                                 mv "${filename}" "${videoname} ${SXX}${j}.mp4"
                                 break 1
-                            elif [[ ${filename} == "${j}.mp4" ]] || [[ ${filename} == "${j}.mkv" ]];then
+                            elif [[ ${filename} == "${j}.mp4" ]] || [[ ${filename} == "${j}.mkv" ]]; then
                                 mv "${filename}" "${videoname} ${SXX}${j}.mp4"
                                 break 1
                             fi
@@ -269,30 +245,28 @@ rename_2_0(){
                             failurekey=1
                             break 1
                         fi
-                        j=$(($j+1))
+                        j=$(($j + 1))
                     done
                 done
             else
                 SXX="S${season:0-2}"
-                for filename in $filenames
-                do
+                for filename in $filenames; do
                     j=0
-                    while :
-                    do
-                        if [ $j -lt '10' ];then
+                    while :; do
+                        if [ $j -lt '10' ]; then
                             jj="0${j}"
-                            if [[ ${filename} =~ "e${jj}" ]] || [[ ${filename} =~ "[${jj}]" ]];then
+                            if [[ ${filename} =~ "e${jj}" ]] || [[ ${filename} =~ "[${jj}]" ]]; then
                                 mv "${filename}" "${videoname} ${SXX}E${jj}.mp4"
                                 break 1
-                            elif [[ ${filename} == "${jj}.mp4" ]] || [[ ${filename} == "${jj}.mkv" ]] || [[ ${filename} == "${j}.mp4" ]];then
+                            elif [[ ${filename} == "${jj}.mp4" ]] || [[ ${filename} == "${jj}.mkv" ]] || [[ ${filename} == "${j}.mp4" ]]; then
                                 mv "${filename}" "${videoname} ${SXX}E${jj}.mp4"
                                 break 1
                             fi
-                        elif [ $j -lt '100' ];then
-                            if [[ ${filename} =~ "e${j}" ]] || [[ ${filename} =~ "[${j}]" ]];then
+                        elif [ $j -lt '100' ]; then
+                            if [[ ${filename} =~ "e${j}" ]] || [[ ${filename} =~ "[${j}]" ]]; then
                                 mv "${filename}" "${videoname} ${SXX}E${j}.mp4"
                                 break 1
-                            elif [[ ${filename} == "${j}.mp4" ]] || [[ ${filename} == "${j}.mkv" ]];then
+                            elif [[ ${filename} == "${j}.mp4" ]] || [[ ${filename} == "${j}.mkv" ]]; then
                                 mv "${filename}" "${videoname} ${SXX}E${j}.mp4"
                                 break 1
                             fi
@@ -302,11 +276,11 @@ rename_2_0(){
                             failurekey=1
                             break 1
                         fi
-                        j=$(($j+1))
+                        j=$(($j + 1))
                     done
                 done
             fi
-            find ./ -type f -print0 | xargs -0 md5sum | sort > "${rootpath}/${videoname}_${season:0-2}_new.txt"
+            find ./ -type f -print0 | xargs -0 md5sum | sort >"${rootpath}/${videoname}_${season:0-2}_new.txt"
             checkmd5
             cd ..
         done
@@ -317,39 +291,36 @@ rename_2_0(){
     deleteuseless
 }
 
-
-startrename(){
+startrename() {
     echo "1.在当前目录执行重命名（xx.mp4）"
     echo "2.对 rename_lyh 文件夹中的剧集进行批量重命名。（格式: rename_lyh/剧名/Season 00/剧名 S00E00.mp4）"
     echo "0.退出"
     IFS_OLD=$IFS
-    while :
-    do
+    while :; do
         read -p "请选择: " key
         case ${key} in
-            1)
-                IFS=$'\r\n'
-                rename_1_0
-                break 1
-                ;;
-            2)
-                IFS=$'\r\n'
-                rename_2_0
-                break 1
-                ;;
-            0)
-                break 1
-                ;;
-            *)
-                echo -e "\033[31;1m [错误] \033[0m 请重新输入"
-                ;;
+        1)
+            IFS=$'\r\n'
+            rename_1_0
+            break 1
+            ;;
+        2)
+            IFS=$'\r\n'
+            rename_2_0
+            break 1
+            ;;
+        0)
+            break 1
+            ;;
+        *)
+            echo -e "\033[31;1m [错误] \033[0m 请重新输入"
+            ;;
         esac
     done
     IFS=$IFS_OLD
 }
 
-
-if [ -d "rename_lyh" ];then
+if [ -d "rename_lyh" ]; then
     echo ""
     echo ""
     startrename
