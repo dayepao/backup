1fa00bfd517a5889b8c0a7efe6f32d44588ac1d2
dayepao <56154142+dayepao@users.noreply.github.com>
2023-08-27 21:29:25 +0800

    Update set_zhcn.sh

--- sh/set_zhcn.sh
+++ sh/set_zhcn.sh
@@ -4,6 +4,7 @@ do
     read -p "是否要设置系统语言为中文?(y/N):" skey
     case ${skey} in
     [yY])
+        sed -i '/#.*en_US.UTF-8 UTF-8/s/^#[[:space:]]*//' /etc/locale.gen && ! grep -qE "^[[:space:]]*en_US.UTF-8 UTF-8" /etc/locale.gen && echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
         sed -i '/#.*zh_CN.UTF-8 UTF-8/s/^#[[:space:]]*//' /etc/locale.gen && ! grep -qE "^[[:space:]]*zh_CN.UTF-8 UTF-8" /etc/locale.gen && echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
         locale-gen
         update-locale "LANG=zh_CN.UTF-8"
