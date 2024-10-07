3a1b0021d9ff7fccc7e1bf004ecfce003c4c81a8
dayepao <56154142+dayepao@users.noreply.github.com>
2024-10-07 22:10:25 +0800

    Update compile_openwrt.sh

--- sh/compile_openwrt.sh
+++ sh/compile_openwrt.sh
@@ -59,8 +59,10 @@ if [ "$dev_flag" == "1" ]; then
     echo -e "${green}Switching to branch: main${plain}"
     git checkout main
 else
-    echo -e "${green}Switching to branch: openwrt-${openwrt_ver%.*}${plain}"
-    git checkout openwrt-${openwrt_ver%.*}
+    # echo -e "${green}Switching to branch: openwrt-${openwrt_ver%.*}${plain}"
+    # git checkout openwrt-${openwrt_ver%.*}
+    echo -e "${green}Switching to tag: v${openwrt_ver}${plain}"
+    git checkout v${openwrt_ver}
 fi
 if [ $? -ne 0 ]; then
     echo -e "${red}Switching failed, exiting the script${plain}"
@@ -126,19 +128,6 @@ if [ "$dev_flag" != "1" ]; then
     echo -e "${green}Updating packages/lang/golang${plain}"
     rm -rf ${compile_path}/feeds/packages/lang/golang
     cp -r ${tmp_path}/packages/lang/golang ${compile_path}/feeds/packages/lang/golang
-
-    # 切换到指定版本
-    # echo -e "${green}Switching to branch: openwrt-${openwrt_ver%.*}${plain}"
-    # git checkout openwrt-${openwrt_ver%.*}
-    # if [ $? -ne 0 ]; then
-    #     echo -e "${red}Switching failed, exiting the script${plain}"
-    #     exit 1
-    # fi
-
-    # 更新 packages/lang/rust 包
-    # echo -e "${green}Updating packages/lang/rust${plain}"
-    # rm -rf ${compile_path}/feeds/packages/lang/rust
-    # cp -r ${tmp_path}/packages/lang/rust ${compile_path}/feeds/packages/lang/rust
 
     cd ${compile_path}
 fi
